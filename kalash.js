08377f6889d371d61a34f6bf03ea6b7e2d6845d0
rehanjagirani <16934653-rehanjagirani@users.noreply.replit.com>
2023-08-19 10:31:25 +0000

    Initial commit

---
+++ kalash.js
@@ -0,0 +1,15 @@
+module.exports = {
+  token: process.env.token,
+  port: "3000",
+  prefix: "!",
+  client: "",
+  client_id: "1142332093397999647",
+  client_secret: "DdS65M5rZC6ZmOBdYds8mQIfu-eMeU-C",
+  redirect_uri: "https://auth-bot-6.rehanjagirani.repl.co",
+  footer: "TellyGamerz",
+  support: "https://discord.gg/rbxxH7sE",
+  wehbook: "https://discord.com/api/webhooks/1142114386396315738/RREyLWPA3NYjBAU7wSOK2hEHkcWbIrLMeQkFlT_Ufk6qN9XCO_HiiUO1XE6dLFFvveZR", //wehbook de logs
+  owners: ["1129101885069082786"],
+  authLink: `https://discord.com/api/oauth2/authorize?client_id=1142332093397999647&redirect_uri=https%3A%2F%2Fauth-bot-6.rehanjagirani.repl.co&response_type=code&scope=identify%20guilds.join`
+}
+
